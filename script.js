84eb97453138dabec1940f194abffa7ae72f2268
matheusassis12-oss <matheus.assis12@escola.pr.gov.br>
2026-04-16 08:28:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// --- GESTÃO DE DADOS ---
+const services = [
+    { title: "Design UI/UX", desc: "Interfaces focadas em conversão e facilidade de uso." },
+    { title: "Auditoria A11y", desc: "Análise completa para conformidade com a WCAG." },
+    { title:
